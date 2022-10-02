e2ac72d285c7c56fd2012ae5a36dca3d1854cb3a
Edshiro <hot.hunter@mail.ru>
2022-10-03 02:48:26 +0600

    2:48 - 03.10.2022

---
+++ GENERAL/Lab_10/MORE_LESS_EQ.sh
@@ -0,0 +1,13 @@
+﻿set +v
+echo Enter first number:
+read a
+echo Enter second number:
+read b
+if [ "$a" -eq "$b" ]; then echo "$a equal $b" 
+fi
+if [ "$a" -gt "$b" ]; then echo "$a more $b" 
+fi
+if [ "$a" -lt "$b" ]; then echo "$a less $b" 
+fi
+
+read
